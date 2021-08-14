4c64b9d33987899eb99cb4dd3f51bc865124d44a
Hugh Ahn <royalahn@gmail.com>
2021-08-14 23:50:45 +0900

    Update sed for .zshrc

--- install.sh
+++ install.sh
@@ -1,35 +1,40 @@
 #!/data/data/com.termux/files/usr/bin/bash
 termux-setup-storage
 
-echo -e "\n\nInstall zsh, git, vim etc \n\n"
+echo -e "\n\n# Install used programs\n\n"
 
 apt update
-apt install -y git zsh vim screenfetch openssh
+apt install -y git zsh vim screenfetch neofetch openssh sshpass
+
+echo -e "\n\n# Copy theme and font\n\n"
 
 mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
 cp -R "./.termux" "$HOME/.termux"
 
-echo -e "\n\nInstall oh-my-zsh \n\n"
+echo -e "\n\n# Install oh-my-zsh\n\n"
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
 mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
 cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
-sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
-sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
 
-echo -e "\n\nInstall oh-my-zsh plugins \n\n"
+echo -e "\n\n# Update .zshrc to change theme to agnoster\n\n"
+
+sed -i 's/robbyrussell/agnoster/' ~/.zshrc
+
+echo -e "\n\n# Install oh-my-zsh plugins\n\n"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-sed -i '/^plugins=(git)/d' "$HOME/.zshrc"
-sed -i '1iplugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)' "$HOME/.zshrc"
+echo -e "\n\n# Update .zshrc to use plugins\n\n"
 
-echo -e "\n\nChange shell to zsh \n\n"
+sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)/' ~/.zshrc
+
+echo -e "\n\n# Change shell to zsh\n\n"
 
 chsh -s zsh
 
-echo -e "Please restart Termux"
+echo -e "# Please restart Termux"
 
 exit
