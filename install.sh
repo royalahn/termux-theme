44adb06c6953792a414b46ddade8c2e7ff32e507
Hugh Ahn <royalahn@gmail.com>
2021-08-14 18:38:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 termux-setup-storage
 
+echo -e "\n\nInstall zsh, git, vim etc \n\n"
+
 apt update
-apt install -y git zsh vim
+apt install -y git zsh vim screenfetch openssh
 
 mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
 cp -R "./.termux" "$HOME/.termux"
 
-echo -e "\n\n\n Install oh-my-zsh \n\n"
+echo -e "\n\nInstall oh-my-zsh \n\n"
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
 mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
@@ -15,7 +17,7 @@ cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
 sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
 sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
 
-echo -e "\n\n\n Install oh-my-zsh plugins \n\n"
+echo -e "\n\nInstall oh-my-zsh plugins \n\n"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -24,10 +26,10 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 sed -i '/^plugins=(git)/d' "$HOME/.zshrc"
 sed -i '1iplugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)' "$HOME/.zshrc"
 
-echo -e "\n\n\n Change shell to zsh \n\n"
+echo -e "\n\nChange shell to zsh \n\n"
 
 chsh -s zsh
 
-echo -e "\n\n\n Please restart Termux app..."
+echo -e "Please restart Termux"
 
 exit
